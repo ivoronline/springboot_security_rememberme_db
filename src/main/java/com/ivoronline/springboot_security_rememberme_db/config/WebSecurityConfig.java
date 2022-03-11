83149a690ab49920a7eb3f7a4dc0a14fe31b4111
igoreta <ivoronline@gmail.com>
2022-03-11 16:48:16 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_security_rememberme_db/config/WebSecurityConfig.java
+++ src/main/java/com/ivoronline/springboot_security_rememberme_db/config/WebSecurityConfig.java
@@ -2,12 +2,15 @@ package com.ivoronline.springboot_security_rememberme_db.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -15,9 +18,18 @@ import org.springframework.security.web.authentication.rememberme.PersistentToke
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   //PROPERTIES
-  @Autowired
-  private final UserDetailsService        userDetailsService;
-  private final PersistentTokenRepository persistentTokenRepository ;
+  @Autowired UserDetailsService        userDetailsService;
+  @Autowired PersistentTokenRepository persistentTokenRepository;
+
+  //=================================================================
+  // PERSISTENT TOKEN REPOSITORY
+  //=================================================================
+  @Bean
+  public PersistentTokenRepository persistentTokenRepository(DataSource dataSource) {
+    JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+                            tokenRepository.setDataSource(dataSource);
+    return tokenRepository;
+  }
 
   //=================================================================
   // CONFIGURE
@@ -30,10 +42,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       .tokenRepository(persistentTokenRepository)
       .userDetailsService(userDetailsService);
 
+    //H2 CONSOLE
+    httpSecurity.authorizeRequests(authorize -> { authorize.antMatchers("/h2-console/**").permitAll(); });
+    httpSecurity.headers().frameOptions().sameOrigin();
+
     //DISABLE CSRF
     httpSecurity.csrf().disable();
 
-    //SECURE EVERYTHING
+    //SECURE ALL RESOURCES
     httpSecurity.authorizeRequests().anyRequest().authenticated();
 
     //DEFAULT LOGIN FORM
